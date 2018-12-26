36b396792c9a4826163d59204c1e96ca077ea685
Alisson <alissonmbr@gmail.com>
2018-12-26 17:19:30 -0200

    implementing tests

--- test/test.js
+++ test/test.js
@@ -1,28 +1,180 @@
 var dirtree = require('../lib');
+var assert = require('assert');
 
-console.log(
-    JSON.stringify(
-        dirtree.dirTojson(__dirname + '/testDir', {
+function walk(dirTree, fn, param) {
+
+    if (dirTree.isDir) {
+        for (var i = 0; i < dirTree.child.length; ++i) {
+            if (!walk(dirTree.child[i], fn, param)) {
+                return false;
+            }
+        }
+    }
+
+    return fn(dirTree, param);
+}
+
+function extensionChecker(node, extension) {
+    return node.isDir || !node.isDir && node.ext === extension;
+}
+
+function fileNameChecker(node, regex) {
+    return node.isDir || !node.isDir && node.name.search(regex) > -1;
+}
+
+function emptyFolderChecker(node) {
+    return !node.isDir || node.isDir && node.child.length > 0;
+}
+
+describe('options', function () {
+    describe('default options', function () {
+        it('should be equal to the json', function () {
+            assert.deepStrictEqual(dirtree.dirTojson(__dirname + '/testDir'), { "ext": "", "isDir": true, "path": "testDir", "name": "testDir", "child": [{ "ext": "", "path": "testDir/dir1", "name": "dir1", "isDir": true, "child": [{ "ext": "css", "path": "testDir/dir1/css1.css", "name": "css1.css", "isDir": false }, { "ext": "txt", "path": "testDir/dir1/file1.txt", "name": "file1.txt", "isDir": false }, { "ext": "txt", "path": "testDir/dir1/file2.txt", "name": "file2.txt", "isDir": false }, { "ext": "html", "path": "testDir/dir1/index.html", "name": "index.html", "isDir": false }, { "ext": "js", "path": "testDir/dir1/js1.js", "name": "js1.js", "isDir": false }] }, { "ext": "", "path": "testDir/dir2", "name": "dir2", "isDir": true, "child": [{ "ext": "txt", "path": "testDir/dir2/file1.txt", "name": "file1.txt", "isDir": false }, { "ext": "txt", "path": "testDir/dir2/file2.txt", "name": "file2.txt", "isDir": false }] }, { "ext": "", "path": "testDir/dir3", "name": "dir3", "isDir": true, "child": [] }, { "ext": "txt", "path": "testDir/file1.txt", "name": "file1.txt", "isDir": false }] });
+        });
+    });
+
+    describe('include/exclude options', function () {
+        describe('includeAbsolutePath', function () {
+            it('should include', function () {
+                assert.ok(!!dirtree.dirTojson(__dirname + '/testDir', { includeAbsolutePath: true }).absolutePath);
+            });
+            it('should exclude', function () {
+                assert.ok(!dirtree.dirTojson(__dirname + '/testDir', { includeAbsolutePath: false }).absolutePath);
+            });
+        });
+
+        describe('includeSize', function () {
+            it('should include', function () {
+                assert.ok(!!dirtree.dirTojson(__dirname + '/testDir', { includeSize: true }).size);
+            });
+            it('should exclude', function () {
+                assert.ok(!dirtree.dirTojson(__dirname + '/testDir', { includeSize: false }).size);
+            });
+        });
+
+        describe('includeCreationTime', function () {
+            it('should include', function () {
+                assert.ok(!!dirtree.dirTojson(__dirname + '/testDir', { includeCreationTime: true }).creationTime);
+            });
+            it('should exclude', function () {
+                assert.ok(!dirtree.dirTojson(__dirname + '/testDir', { includeCreationTime: false }).creationTime);
+            });
+        });
+
+        describe('includeModificationTime', function () {
+            it('should include', function () {
+                assert.ok(!!dirtree.dirTojson(__dirname + '/testDir', { includeModificationTime: true }).modificationTime);
+            });
+            it('should exclude', function () {
+                assert.ok(!dirtree.dirTojson(__dirname + '/testDir', { includeModificationTime: false }).modificationTime);
+            });
+        });
+
+        describe('includeDirFlag', function () {
+            it('should include', function () {
+                assert.ok(!!dirtree.dirTojson(__dirname + '/testDir', { includeDirFlag: true }).isDir);
+            });
+            it('should exclude', function () {
+                assert.ok(!dirtree.dirTojson(__dirname + '/testDir', { includeDirFlag: false }).isDir);
+            });
+        });
+
+        describe('includeExtension', function () {
+            it('should include', function () {
+                assert.ok(dirtree.dirTojson(__dirname + '/testDir', { includeExtension: true }).ext !== undefined);
+            });
+            it('should exclude', function () {
+                assert.ok(dirtree.dirTojson(__dirname + '/testDir', { includeExtension: false }).ext === undefined);
+            });
+        });
+
+    });
+
+    describe('root override', function () {
+        it('should override root path', function () {
+            assert.strictEqual(dirtree.dirTojson(__dirname + '/testDir', { rootPath: "." }).path, ".");
+        });
+
+        it('should override root name', function () {
+            assert.strictEqual(dirtree.dirTojson(__dirname + '/testDir', { rootName: "root" }).name, "root");
+        });
+    });
+
+    describe('attribute names override', function () {
+        var overrideTest = dirtree.dirTojson(__dirname + '/testDir', {
             includeAbsolutePath: true,
             includeSize: true,
             includeCreationTime: true,
             includeModificationTime: true,
-            filter: {
-                folderName: /dir1/i,
-                fileExtension: /css|js/i
-            },
+            includeDirFlag: true,
+            includeExtension: true,
             attributeName: {
-                "absolutePath": "absolutePath2",
-                "creationTime": "creationTime2",
-                "modificationTime": "modificationTime2",
-                "path": "path2",
-                "fileName": "name2",
-                "extension": "ext2",
-                "dirFlag": "isDir2",
-                "size": "size2"
+                path: "path2",
+                child: "child2",
+                absolutePath: "absolutepath2",
+                fileName: "name2",
+                extension: "ext2",
+                dirFlag: "isdir2",
+                creationTime: "creationtime2",
+                modificationTime: "modificationtime2",
+                size: "size2"
             }
-        }),
-        null,
-        2
-    )
-);
+        });
+
+        it('should override path attribute', function () {
+            assert.notStrictEqual(overrideTest.path2, undefined);
+        });
+
+        it('should override child attribute', function () {
+            assert.notStrictEqual(overrideTest.child2, undefined);
+        });
+
+        it('should override absolutepath attribute', function () {
+            assert.notStrictEqual(overrideTest.absolutepath2, undefined);
+        });
+
+        it('should override filename attribute', function () {
+            assert.notStrictEqual(overrideTest.name2, undefined);
+        });
+
+        it('should override extension attribute', function () {
+            assert.notStrictEqual(overrideTest.ext2, undefined);
+        });
+
+        it('should override dirflag attribute', function () {
+            assert.notStrictEqual(overrideTest.isdir2, undefined);
+        });
+
+        it('should override creationtime attribute', function () {
+            assert.notStrictEqual(overrideTest.creationtime2, undefined);
+        });
+
+        it('should override size attribute', function () {
+            assert.notStrictEqual(overrideTest.size2, undefined);
+        });
+
+        it('should override modificationtime attribute', function () {
+            assert.notStrictEqual(overrideTest.modificationtime2, undefined);
+        });
+
+    });
+
+    describe('filter', function () {
+        it('should list just html files', function () {
+            assert.ok(walk(dirtree.dirTojson(__dirname + '/testDir', { filter: { fileExtension: "html" } }), extensionChecker, 'html'));
+            assert.ok(!walk(dirtree.dirTojson(__dirname + '/testDir', { filter: { fileExtension: "js" } }), extensionChecker, 'html'));
+        });
+
+        it('should list just files with the name "/file.*/"', function () {
+            assert.ok(walk(dirtree.dirTojson(__dirname + '/testDir', { filter: { fileName: /^file.*$/ } }), fileNameChecker, /^file.*$/));
+            assert.ok(!walk(dirtree.dirTojson(__dirname + '/testDir', { filter: { fileName: /^css.*$/ } }), fileNameChecker, /^file.*$/));
+        });
+    });
+
+    describe('excludeEmptyFolders', function () {
+        it('shoult exclude empty folders', function () {
+            assert.ok(walk(dirtree.dirTojson(__dirname + '/testDir', { excludeEmptyFolders: true }), emptyFolderChecker));
+        });
+    });
+
+});
